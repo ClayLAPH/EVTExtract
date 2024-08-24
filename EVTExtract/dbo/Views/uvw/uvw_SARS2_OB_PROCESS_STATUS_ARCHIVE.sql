2c30af0d55068871d5f0895175b914739b46664c
LAPHClay <cborkholm@ph.lacounty.gov>
2024-08-24 11:53:43 -0400

    Includes both archive 1 and 2 (distinguisshed by archive number) in the uvw_SARS2 archive views.

--- EVTExtract/dbo/Views/uvw/uvw_SARS2_OB_PROCESS_STATUS_ARCHIVE.sql
+++ EVTExtract/dbo/Views/uvw/uvw_SARS2_OB_PROCESS_STATUS_ARCHIVE.sql
@@ -11,10 +11,29 @@ select
   P.AUD_Username                                             Username,
   ArchiveVersion                                           = 1
 from    
-  dbo.SARS2_OUTBREAK_PROCESS_STATUS_HISTORY_ARCHIVE as P with (nolock) 
+  dbo.SARS2_OUTBREAK_PROCESS_STATUS_HISTORY_ARCHIVE as P 
 	inner join
-  dbo.SARS2_OUTBREAK_ARCHIVE as O with (nolock)
+  dbo.SARS2_OUTBREAK_ARCHIVE as O
   on 
     P.OUTB_RowID = O.OUTB_RowID
 
+union all 
+
+select
+  P.OUTB_RowID                                               Row_ID,
+  O.OUTB_OUTBREAKID                                          Outbreak_ID,
+  O.OUTB_OutbreakNumber                                      Outbreak_Number,
+  O.OUTB_OutbreakLocation                                    Location,
+  P.AUD_ID                                                   AUD_ID,
+  P.AUD_OldValue                                             Old_Value,
+  P.AUD_NewValue                                             New_Value,
+  try_convert(datetime,P.AUD_ActionDate)                     Action_Date,
+  P.AUD_Username                                             Username,
+  ArchiveVersion                                           = 2
+from    
+  dbo.SARS2_OUTBREAK_PROCESS_STATUS_HISTORY_ARCHIVE2 as P 
+	inner join
+  dbo.SARS2_OUTBREAK_ARCHIVE2 as O
+  on 
+    P.OUTB_RowID = O.OUTB_RowID
 
--- EVTExtract/dbo/Views/uvw/uvw_SARS2_SPECIMEN_ARCHIVE.sql
+++ EVTExtract/dbo/Views/uvw/uvw_SARS2_SPECIMEN_ARCHIVE.sql
@@ -9,4 +9,18 @@ select
   ArchiveVersion          = 1
 
 from
-  dbo.SARS2_SPECIMEN_ARCHIVE        c with (nolock)
+  dbo.SARS2_SPECIMEN_ARCHIVE        c 
+
+union all
+
+select
+  Incident_ID             = c2.[PR_INCIDENTID],
+  Specimen_Collected_Date = c2.[Specimen Collected Date],
+  Specimen_Received_Date  = c2.[Specimen Received Date],
+  Result                  = c2.[Result],
+  Specimen_Notes          = c2.[Specimen Notes],
+  Specimen_Types          = c2.[Specimen Types],
+  ArchiveVersion          = 2
+
+from
+  dbo.SARS2_SPECIMEN_ARCHIVE2        c2;
